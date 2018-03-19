5408787440f8db722b02729a79e2118ec4bac262
Felix <Felix@example.com>
2018-03-19 16:50:12 +0800

    Creating markup on the fly

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -11,6 +11,34 @@ function addLoadEvent(func) {
     }
 }
 
+// 在目标元素之后插入一个新元素
+function insertAfter(newElement,targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/placeholder.gif");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+}
+
 function showPic(whichpic) {
     if (!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
@@ -23,11 +51,11 @@ function showPic(whichpic) {
     }
     return true;
 }
-
+/* 
 function countBodyChildren() {
     var body_element = document.getElementsByTagName("body")[0];
     alert(body_element.childNodes.length);
-}
+} */
 
 function prepareGallery() {
     if (!document.getElementsByTagName) return false;
@@ -43,4 +71,5 @@ function prepareGallery() {
     }
 }
 
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
